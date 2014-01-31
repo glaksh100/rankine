c3fed3d2793234d586517d01eb1b7bf341365b56
Lakshmi <glaksh100@gmail.com>
2014-01-31 16:00:41 -0600

    Corrected syntax error

--- grab_forecast_test.py
+++ grab_forecast_test.py
@@ -1,7 +1,7 @@
 
 import requests
 
-def get_raw_data_from_url()
+def get_raw_data_from_url():
 	site='http://www.nws.noaa.gov/mdl/gfslamp/docs/stations_info.shtml'
 	page= requests.get(site)
 	return page.text
